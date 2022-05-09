85d48de9c268011bdcaa9723edeeaefd14f5a054
Bilal Dastagir <bilzkaist@gmail.com>
2022-05-09 21:28:42 +0900

    createHmtl

--- nftkard/create.js
+++ nftkard/create.js
@@ -16,11 +16,50 @@ function deletePrevious(){
 
 function addNew(){
     if(getSelectedValue() == 0) alert("Please choose number of images you want to generate");
+    
+    let keywordss = document.getElementById("keywords").value;
+    if(keywordss.length == 0) alert("Please enter your search keywords");
+
     let numbOfImages = parseInt(getSelectedValue());
     document.getElementById("footerPart").style.marginTop = "0px"
-    for(let i = 0; i < numbOfImages; i++){
+
+let requestOptions = {
+      method: 'POST',
+      headers : {'Content-Type': 'application/json'},
+      body: JSON.stringify({ search:  keywordss})
+  };
+
+    let uri = 'http://143.248.56.39:50000/ai_generator'
+    fetch(uri, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      loadNFTImages(data);
+    });
+
+
+
+}
+
+
+
+function findID(clickedID){
+    return clickedID.id;
+}
+
+function listBtn(btnId) {
+	console.log(btnId)
+}
+
+
+const loadNFTImages = async (data) => { 
+
+console.log(data)
+imageItems = data.items
+console.log(imageItems)
+for(var i = 0; i < imageItems.length; i++) {
+
         
-        let pictureURL = "http://143.248.56.39:5000/imgs/NFT" + (i+1).toString();
+        let pictureURL = imageItems[i].address;
         let divOfPicture = document.createElement("div");
         let NFTimage = document.createElement("img");
         let imgID = (i+1).toString() + "NFT";
@@ -39,11 +78,11 @@ function addNew(){
         button.setAttribute("class","buybutton");
         button.setAttribute("id",buttonName);
 
+	let jsonURL = imageItems[i].json
+
+	button.addEventListener("click", function(){listBtn(jsonURL)});
+
        
     }
-    if(numbOfImages > 0 && numbOfImages <= 3)document.getElementById("footerPart").style.marginTop = "20px";
-}
-
-function findID(clickedID){
-    return clickedID.id;
+    if(imageItems.length > 0 && imageItems.length <= 10)document.getElementById("footerPart").style.marginTop = "20px";
 }
