790a283c8efb91559499e463974b39b7b1101bb0
umartechboy <umar.techboy@gmail.com>
2022-05-23 19:24:59 +0500

    mint code login moved to common file

--- nftkard/js/explore.js
+++ nftkard/js/explore.js
@@ -1,3 +1,68 @@
+Moralis.initialize("PaClcWW3CtFlc141AaQM2AECASWAKoymybcvetWC"); // Application id from moralis.io
+Moralis.serverURL = "https://7mo2aen51dyh.usemoralis.com:2053/server"; //Server url from moralis.io
+
+const nft_contract_address = "0x0Fb6EF3505b9c52Ed39595433a21aF9B5FCc4431" //NFT Minting Contract Use This One "Batteries Included", code of this contract is in the github repository under contract_base for your reference.
+/*
+Available deployed contracts
+Ethereum Rinkeby 0x0Fb6EF3505b9c52Ed39595433a21aF9B5FCc4431
+Polygon Mumbai 0x351bbee7C6E9268A1BF741B098448477E08A0a53
+BSC Testnet 0x88624DD1c725C6A95E223170fa99ddB22E1C6DDD
+*/
+
+console.log("mint scripts")
+window.userWalletAddress = null;
+var metaMaskAvailable = false;
+const loginButton = document.getElementById('loginButton');
+
+async function LoginMetaMaskClick(){
+    if (!metaMaskAvailable)
+    return;
+    await login();
+}
+async function loginWithMetaMask() {
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+          .catch((e) => {
+              console.error(e.message)
+              return
+          })
+      if (!accounts) { return }
+
+      window.userWalletAddress = accounts[0];
+  }
+function toggleButton() {
+    if (!window.ethereum) {
+        loginButton.innerText = 'MetaMask is not installed'
+        return false
+    }
+    else
+        metaMaskAvailable = true;
+    loginComplete();
+}
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    toggleButton()
+});
+
+const web3 = new Web3(window.ethereum);
+
+//frontend logic
+
+function loginComplete()
+{
+  metaMaskLoginControls.remove();
+  controlsContainer.append(uploadControlsBkp);
+  console.log("controls changed");
+}
+async function login(){
+  document.getElementById('loginButton').setAttribute("disabled", null);
+  Moralis.Web3.authenticate().then(function (user) {
+      user.save();
+      console.log("user saved");
+      loginComplete();
+  })
+}
+
 openFileChild = null;
 var selectedFile = null;
 
